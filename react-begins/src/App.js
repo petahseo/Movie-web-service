8f6a903752075b0204868d9b2a1e093b57dfd44e
petahseo <94450217+petahseo@users.noreply.github.com>
2022-07-21 17:56:54 +0900

    practice - To Do List

--- react-begins/src/App.js
+++ react-begins/src/App.js
@@ -2,13 +2,19 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("i run all the time")
   useEffect(() => {
     console.log("Call the API...")
   }, []);
+  useEffect(() => {
+    console.log("SEARCH FOR", keyword)
+  }, [keyword]);
   return (
     <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
       <h1>{counter}</h1>
       <button onClick={onClick} >Click me</button>
     </div>
--- react-begins/src/index.js
+++ react-begins/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Todo from './ToDoList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-    <App />
-
+    <div>
+        <App />
+        <hr/>
+        <Todo />
+    </div>
 );
