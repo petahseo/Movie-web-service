77ba266a77e4bf4317a097e7faae17de477405b1
petahseo <94450217+petahseo@users.noreply.github.com>
2022-07-26 01:30:37 +0900

    finishing up react course

--- react-begins/src/Movie.js
+++ react-begins/src/Movie.js
@@ -8,7 +8,7 @@ function Movie() {
   return (
     <Router>
       <Routes>
-        <Route path="movie" element={<Detail/>}/>
+        <Route path="movie/:id" element={<Detail/>}/>
         <Route path="/" element={<Home/>}/>
       </Routes>
     </Router>
